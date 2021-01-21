e29cda4ab16585b9bcb82f9788d80876985aa02b
namratha <namrathanm2612@gmail.com>
2021-01-21 17:19:51 +0530

    manage-assets

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,22 +16,21 @@ export class MapComponent implements OnInit {
     var center = [-33.865, 151.2094];
 
     // Create the map
-    this.map = L.map('map').setView(center, 5);
+    this.map = L.map('map').setView(center, 8);
 
     // Set up the OSM layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        'Data © <a href="http://osm.org/copyright">OpenStreetMap</a>',
-      maxZoom: 18,
-    }).addTo(this.map);
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     'Data © <a href="http://osm.org/copyright">OpenStreetMap</a>',
+    //   maxZoom: 18,
+    // }).addTo(this.map);
 
     // add a marker in the given location
-    L.marker(center).addTo(this.map);
-    L.marker([-35.865, 154.2094]).addTo(this.map);
-
-    var imageUrl =
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Sydney_Opera_House_-_Dec_2008.jpg/1024px-Sydney_Opera_House_-_Dec_2008.jpg',
-      imageBounds = [center, [-35.865, 154.2094]];
+    // L.marker(center).addTo(this.map);
+    // L.marker([-35.865, 154.2094]).addTo(this.map);
+   
+    var imageUrl ='../../assets/office-layout.png',
+              imageBounds = [center, [-35.865, 154.2094]];
 
     L.imageOverlay(imageUrl, imageBounds).addTo(this.map);
   }
