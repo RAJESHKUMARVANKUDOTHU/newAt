c78c4118a591f009b959cd7979ab4eea4a66af06
roshan <roshansuvarnkar908@gmail.com>
2021-02-24 10:55:25 +0530

    dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiHost:'http://15.207.248.70:3000',
+  apiHost:'http://3.7.17.108:3000',
   production: false
 };
 
